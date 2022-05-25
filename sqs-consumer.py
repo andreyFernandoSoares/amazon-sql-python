35435c2439790d088cc2f467e2f7083656d60cc7
andreyFernadoSoares <67378685+andreyFernandoSoares@users.noreply.github.com>
2022-05-24 21:09:06 -0300

    Update sqs-consumer.py

--- sqs-consumer.py
+++ sqs-consumer.py
@@ -2,13 +2,13 @@ import boto3
 
 sqs = boto3.resource('sqs')
 
-queue_name = 'cinema-queue'
+queue_name = 'movie'
 
 def get_queue():
     queue = sqs.get_queue_by_name(QueueName=queue_name)
     return queue
 
-def receive_messages():
+def lambda_handler(event, context):
     queue = get_queue()
 
     messages = queue.receive_messages(
@@ -19,6 +19,3 @@ def receive_messages():
 
     for msg in messages:
         print(msg.body)
-
-if __name__ == '__main__':
-    receive_messages()
